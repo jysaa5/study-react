c00bc1524a642badaef27ee820161f43ea6cabf9
jysaa5 <jysaa5@naver.com>
2026-01-29 21:13:30 +0900

    header css 설정

--- live-coding-test/src/shared/Header.tsx
+++ live-coding-test/src/shared/Header.tsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router'
 export default function Header() {
 
     return (
-        <header>
-            <Link to="/">Home</Link>
-            <h1>Live Coding Test</h1>
-        </header>
-    )
+      <header className="flex items-center px-6 py-4 bg-white border-b border-gray-100 shadow-sm sticky top-0 z-50">
+        <Link to="/" className="text-blue-600 font-bold hover:text-blue-800 transition-colors">
+          Home
+        </Link>
+        <div className="grow">
+          <h1>Live Coding Test</h1>
+        </div>
+      </header>
+    );
 }
