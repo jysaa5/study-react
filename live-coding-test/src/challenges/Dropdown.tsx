4f8f954906e395044c9cf12da9d56cbab81ada0a
jysaa5 <jysaa5@naver.com>
2026-02-09 18:03:16 +0900

    드롭다운 컴포넌트에서 드롭다운이 열렸을 때 바깥 영역을 클릭하면 자동으로 닫히는 기능 추가. 안에 컨텐츠 눌렀을 때도 닫히도록 추가

--- live-coding-test/src/challenges/Dropdown.tsx
+++ live-coding-test/src/challenges/Dropdown.tsx
@@ -2,14 +2,16 @@
 // Create a dropdown menu component that displays a list of items when clicked.
 
 import Layout from '../shared/Layout';
-import { useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 const DropdownContent = ({
   items,
   isOpen,
+  onClose,
 }: {
   items: string[];
   isOpen: boolean;
+  onClose: () => void;
 }) => {
   if (!isOpen) return null;
   return (
@@ -18,6 +20,7 @@ const DropdownContent = ({
         <li
           key={item}
           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer"
+          onClick={onClose}
         >
           {item}
         </li>
@@ -28,19 +31,43 @@ const DropdownContent = ({
 
 const Dropdown = () => {
   const items = ['Profile', 'Settings', 'Logout'];
+
   const [isOpen, setIsOpen] = useState(false);
+
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const handleToggle = () => {
     setIsOpen(!isOpen);
   };
 
+  const close = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <Layout>
-      <div className="relative inline-block text-left">
+      <div className="relative inline-block text-left" ref={dropdownRef}>
         <button type="button" className="btn-primary" onClick={handleToggle}>
           Dropdown
         </button>
-        <DropdownContent items={items} isOpen={isOpen} />
+        <DropdownContent items={items} isOpen={isOpen} onClose={close} />
       </div>
     </Layout>
   );
